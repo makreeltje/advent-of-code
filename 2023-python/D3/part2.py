1686e4252bf1df83d410d0b76dad318fc34648b7
Rick Meels <rick.meels@gmail.com>
2023-12-03 20:22:03 +0100

    Solution for day 3

--- 2023-python/D3/part2.py
+++ 2023-python/D3/part2.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 input_file = open("input.txt", "r")
 input_list = input_file.read().split("\n")
 result = 0
@@ -9,7 +11,7 @@ numbers = []
 for x in range(rows):
     y = 0
     while y < columns:
-        if input_list[x][y].isdecimal():
+        if input_list[x][y].isdigit():
             start = y
             number = ""
             while y < columns and input_list[x][y].isdigit():
@@ -20,17 +22,24 @@ for x in range(rows):
 
         y += 1
 
+gears = {}
 for number in numbers:
-    part_number = False
     for x in range(number[1][0] - 1, number[1][0] + 2):
         if 0 <= x < rows:
             for y in range(number[1][1] - 1, number[1][2] + 2):
                 if 0 <= y < columns:
-                    if not (input_list[x][y].isdigit() or input_list[x][y] == "."):
-                        part_number = True
-                        result += number[0]
-                        break
-            if part_number:
-                break
+                    if input_list[x][y] == "*":
+                        if not gears.get((x, y)):
+                            gears[(x, y)] = []
+                        gears[(x, y)].append(number[0])
+
+for gear in gears:
+    if len(gears[gear]) == 2:
+        product = 1
+        for num in gears[gear]:
+            product *= num
+        result += product
 
 print(result)
+
+
